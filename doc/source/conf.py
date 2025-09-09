b9c950d2065862a5a2dd52d4aee18371e70f654c
li12242 <li12242@tju.edu.cn>
2025-09-08 22:28:34 -0400

    refactor: add sphinx document

---
+++ doc/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+
+project = 'Mat4FVCOM'
+copyright = '2025, li12242'
+author = 'li12242'
+release = 'v1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+matlab_src_dir = os.path.abspath('../src')
+
+extensions = [
+    'sphinxcontrib.matlab', 
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'recommonmark'
+    ]
+primary_domain = "mat"
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+mathjax_path = "es5/tex-chtml.js"
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
